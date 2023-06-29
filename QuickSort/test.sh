94060f73f0f9a6220fe8a9de257a2dbac5bb93e7
Luca Ceragioli <ceragioli_luca@libero.it>
2023-06-29 15:19:27 +0200

    Added multilenght test

--- QuickSort/test.sh
+++ QuickSort/test.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-# Lenght of input list
-INPUT_LIST_LEN=500000
-
 rm -f ./result.csv
-echo "Haskell, C" >> ./result.csv
+echo "Array lenght, Haskell [s], C [s]" >> ./result.csv
 
-for i in {1..10}; do
+for list_len in 1000 10000 100000 1000000; do
+    for i in {1..10}; do
+        # Clear list if existing
+        rm -f ./input.txt
 
-    # Clear list if existing
-    rm -f ./input.txt
+        # Generate some random numbers
+        python3 ./rand.py $list_len "./input.txt"
 
-    # Generate some random numbers
-    python3 ./rand.py $INPUT_LIST_LEN "./input.txt"
+        exec 3>&1 4>&2
+        time_hs=$( TIMEFORMAT="%R"; { time ./quicksort_hs 1>&3 2>&4; } 2>&1)
+        exec 3>&- 4>&-
 
-    exec 3>&1 4>&2
-    time_hs=$( TIMEFORMAT="%R"; { time ./quicksort_hs 1>&3 2>&4; } 2>&1)
-    exec 3>&- 4>&-
+        exec 3>&1 4>&2
+        time_c=$( TIMEFORMAT="%R"; { time ./quicksort_c 1>&3 2>&4; } 2>&1)
+        exec 3>&- 4>&-
 
-    exec 3>&1 4>&2
-    time_c=$( TIMEFORMAT="%R"; { time ./quicksort_c 1>&3 2>&4; } 2>&1)
-    exec 3>&- 4>&-
-
-    echo "$time_hs, $time_c" >> ./result.csv
-
+        echo "$list_len, $time_hs, $time_c" >> ./result.csv
+    done
 done
 
